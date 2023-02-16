b1e66c09c932581b77f60001ad54d8c81124432b
ebenezzeo <125495136+ebenezzeo@users.noreply.github.com>
2023-02-16 06:29:16 +0000

    Update check_answers.php

--- check_answers.php
+++ check_answers.php
@@ -11,10 +11,7 @@ include('dbcon.php');
 				 $option = $_POST['option'];  
 
 // $answer=$_POST['answer_'.$iterate];
- 
-						
- 	
-				
+  			
 for($i=0; $i < count($_POST['mem_id']); $i++){
 		 
 		 $data[$_POST['mem_id'][$i]]['numbering'] = $_POST['numbering'][$i];
@@ -27,18 +24,21 @@ for($i=0; $i < count($_POST['mem_id']); $i++){
 					
 
 	foreach($data as $key => $value){
+		
+		// to comepare selected option to answer 
 	
            if ($_POST['answer'] == $_POST['option'] ){
 			   
-			   
+			//if answer is the same as option mark 1 and and save the selected option by user 
+			
 			$result = mysqli_query($con,"INSERT INTO answers(date,qtn,qtn2,answer,mark,yansa) VALUES 
 	                                            (CURDATE(),'$value[qtn]','$value[numbering]','$value[answer]','1','$option')")or die(mysqli_error($con)); 	//change 3x		
-					} else {	
+					} else {
+
+//else mark 0 if not and save the selected option by user 						
 			$result = mysqli_query($con,"INSERT INTO answers(date,qtn,qtn2,answer,mark,yansa) VALUES 
 	                                             (CURDATE(),'$value[qtn]','$value[numbering]','$value[answer]','0','$option')")or die(mysqli_error($con)); 	//change 3x	
 												}
 			 }
-		  
-		  
-	 		 			  	
+ 	 			  	
 ?>
